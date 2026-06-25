a2b66fee8960982177f715f12c7fecd100bf236b
Amjad Khalfan <i4mjad@gmail.com>
2026-06-25 16:49:28 +0400

    Initial Commit

---
+++ Mudlej/MudlejApp.swift
@@ -0,0 +1,17 @@
+//
+//  MudlejApp.swift
+//  Mudlej
+//
+//  Created by Amjad Khalfan on 25/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MudlejApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
